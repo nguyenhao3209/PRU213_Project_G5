2b1306f1d4d2c3a45c3e0372a6a9aac6afabde32
MinhKhanh1204 <khanhtmce180220@fpt.edu.vn>
2025-10-18 14:49:44 +0700

    first commit

---
+++ Assets/Scripts/UI/LevelComplete.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class LevelComplete : MonoBehaviour
+{
+    public GameObject levelCompleteUI;
+    public string nextLevelName;
+    public string menuSceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision with: " + collision.name);
+
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("PLAYER REACHED GOAL");
+            Time.timeScale = 0f;
+            levelCompleteUI.SetActive(true);
+        }
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1f;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nextLevelName);
+    }
+
+    public void Quit()
+    {
+        Time.timeScale = 1f;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(menuSceneName);
+    }
+}
---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // để load scene
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Load Level1 (hoặc tên scene game của mày)
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ContinueGame()
+    {
+        // Ví dụ: load level đã lưu từ PlayerPrefs
+        int level = PlayerPrefs.GetInt("SavedLevel", 1);
+        SceneManager.LoadScene("Level" + level);
+    }
+
+    public void OpenOptions()
+    {
+        Debug.Log("Options Menu opened!");
+        // Hiện panel options (âm thanh, music) ở đây
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit game!");
+    }
+}
