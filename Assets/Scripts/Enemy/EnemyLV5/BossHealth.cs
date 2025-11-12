a74193462f813939a39e1f944f8b2bbb59ba8052
nguyenhao3209 <haontce180451@fpt.edu.vn>
2025-11-13 01:55:21 +0700

    fix

---
+++ Assets/Scripts/Enemy/EnemyLV5/BossHealth.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealth : MonoBehaviour
+{
+    public int maxHealth = 500;
+    private int currentHealth;
+    public Slider healthBar;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        currentHealth = maxHealth;
+
+        if (healthBar != null)
+        {
+            healthBar.maxValue = maxHealth;
+            healthBar.value = currentHealth;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth < 0) currentHealth = 0;
+
+        if (healthBar != null)
+            healthBar.value = currentHealth;
+
+        anim.SetTrigger("Hit");
+
+        if (currentHealth == 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        anim.SetTrigger("Death");
+        GetComponent<Collider2D>().enabled = false;
+        Destroy(gameObject, 1.5f); // boss biến mất sau 1.5 giây
+    }
+}
